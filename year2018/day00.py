9c48676d9edf99535ca5f34db79fe3a1773708b2
Fei Gao <contact@feigao.org>
2018-12-04 10:30:58 +0800

    update template to use argv input

--- year2018/day00.py
+++ year2018/day00.py
@@ -1,3 +1,5 @@
+import sys
+
 from common import load_input
 
 
@@ -12,10 +14,9 @@ def fn_p2(raw: str):
 if __name__ == "__main__":
     example = """
     """.strip()
-    input_data = load_input(__file__.split(".")[0] + "_in.txt").strip()
-
     # print("Part 1 example:", fn_p1(example))
-    print("Part 1:", fn_p1(input_data))  # answer:
-
     # print("Part 2 example:", fn_p2(example))
+
+    input_data = load_input(sys.argv[1]).strip()
+    print("Part 1:", fn_p1(input_data))  # answer:
     print("Part 2:", fn_p2(input_data))  # answer:
--- year2018/day02.py
+++ year2018/day02.py
@@ -1,9 +1,12 @@
-from common import load_input
+import sys
 from collections import Counter
+from common import load_input
 
 
 def fn_p1(raw: str):
-    two_three = [(2 in c.values(), 3 in c.values()) for c in map(Counter, raw.splitlines())]
+    two_three = [
+        (2 in c.values(), 3 in c.values()) for c in map(Counter, raw.splitlines())
+    ]
     return len([x for x in two_three if x[0] == True]) * len(
         [x for x in two_three if x[1] == True]
     )
@@ -14,7 +17,7 @@ def fn_p2(raw: str):
     for i in range(len(lines[0]) - 1):
         seen = set()
         for line in lines:
-            cut = line[:i] + line[i+1:]
+            cut = line[:i] + line[i + 1 :]
             if cut in seen:
                 return cut
             else:
@@ -25,10 +28,9 @@ def fn_p2(raw: str):
 if __name__ == "__main__":
     example = """
     """.strip()
-    input_data = load_input(__file__.split(".")[0] + "_in.txt").strip()
-
     # print("Part 1 example:", fn_p1(example))
-    print("Part 1:", fn_p1(input_data))  # 5478
-
     # print("Part 2 example:", fn_p2(example))
+
+    input_data = load_input(sys.argv[1]).strip()
+    print("Part 1:", fn_p1(input_data))  # 5478
     print("Part 2:", fn_p2(input_data))  # qyzphxoiseldjrntfygvdmanu
